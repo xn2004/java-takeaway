261de15c45f0a88505c4076b1df6866a1da23140
徐楠 <772913365>
2023-11-14 16:56:23 +0800

    客户催单业务代码开发

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -108,7 +108,17 @@ public class OrderController {
     }
 
 
-
+    /**
+     * 客户催单
+     * @param id
+     * @return
+     */
+    @GetMapping("/reminder/{id}")
+    @ApiOperation("客户催单")
+    public Result reminder(@PathVariable("id") Long id){
+        orderService.reminder(id);
+        return Result.success();
+    }
 
 
 

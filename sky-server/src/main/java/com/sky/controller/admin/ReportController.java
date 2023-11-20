77b8c07414f237abf6c91de832772e4579afac66
徐楠 <772913365>
2023-11-20 22:12:26 +0800

    销量排名统计业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -3,6 +3,7 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
@@ -30,19 +31,21 @@ public class ReportController {
 
     /**
      * 营业额统计
+     *
      * @return
      */
     @GetMapping("/turnoverStatistics")
     @ApiOperation("营业额统计")
     public Result<TurnoverReportVO> turnoverStatistics(@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
-                                                       @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+                                                       @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
 
-        log.info("营业额数据统计:{},{}",begin,end);
-        return Result.success(reportService.getTurnoverStatistics(begin,end));
+        log.info("营业额数据统计:{},{}", begin, end);
+        return Result.success(reportService.getTurnoverStatistics(begin, end));
     }
 
     /**
      * 用户统计
+     *
      * @param begin
      * @param end
      * @return
@@ -50,13 +53,14 @@ public class ReportController {
     @GetMapping("/userStatistics")
     @ApiOperation("用户统计")
     public Result<UserReportVO> userStatistics(@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
-                                               @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
-        log.info("用户数据统计:{},{}",begin,end);
-        return Result.success(reportService.getUserStatistics(begin,end));
+                                               @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
+        log.info("用户数据统计:{},{}", begin, end);
+        return Result.success(reportService.getUserStatistics(begin, end));
     }
 
     /**
      * 订单统计
+     *
      * @param begin
      * @param end
      * @return
@@ -64,9 +68,24 @@ public class ReportController {
     @GetMapping("/ordersStatistics")
     @ApiOperation("订单统计")
     public Result<OrderReportVO> ordersStatistics(@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
-                                                @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
-        log.info("订单数据统计:{},{}",begin,end);
-        return Result.success(reportService.getOrderStatistics(begin,end));
+                                                  @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
+        log.info("订单数据统计:{},{}", begin, end);
+        return Result.success(reportService.getOrderStatistics(begin, end));
     }
 
+
+    /**
+     * 销量排名top10
+     *
+     * @param begin
+     * @param end
+     * @return
+     */
+    @GetMapping("/top10")
+    @ApiOperation("销量排名top10")
+    public Result<SalesTop10ReportVO> top10(@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+                                            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
+        log.info("销量排名top10:{},{}", begin, end);
+        return Result.success(reportService.getSalesTop10(begin, end));
+    }
 }
